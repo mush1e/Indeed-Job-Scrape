ea734bbad42dba7b9936a2195b5fb1e5be67b9bd
Mustafa Siddiqui <mustafa.a.r.siddiqui@outlook.com>
2022-04-09 13:09:13 -0400

    setup virtualenv

--- main.py
+++ main.py
@@ -12,9 +12,12 @@ class idkwhatiwannado:
             print(f"{'what would you like to do today?' : ^50}")
             print()
             print("1) Get weather updates for the day? :3")
+            print("2) Get job listings from random python website? :3")
             self.inp = input()
             if self.inp == '1':
                 print('yay!')
+            if self.inp == '2':
+                print("yay yo")
             break;
 
 if __name__ == '__main__':
