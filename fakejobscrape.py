3689a63d7f0abc0bc2bd92710fcb649ffa311dfc
Mustafa Siddiqui <mustafa.a.r.siddiqui@outlook.com>
2022-04-10 18:40:13 -0400

    watch me Scrape :D

--- fakejobscrape.py
+++ fakejobscrape.py
@@ -6,8 +6,9 @@ class fakejobscrape:
         self.URL="https://realpython.github.io/fake-jobs/"
         page = requests.get(self.URL)
         soup = (BeautifulSoup(page.content, "html.parser"))
-        titles = soup.find("h2", class_="title is-5")
-        print(titles.text)
+        titles = soup.find_all(class_="media-content")
+        for title in titles:
+            print(title.text)
     
 if __name__ == '__main__':
     ob = fakejobscrape()
