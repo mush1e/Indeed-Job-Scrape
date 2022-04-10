60a281c8fc403f7e9c689cc15d5472c804973f76
Mustafa Siddiqui <mustafa.a.r.siddiqui@outlook.com>
2022-04-10 18:35:30 -0400

    extracting texts from html

--- fakejobscrape.py
+++ fakejobscrape.py
@@ -1,10 +1,13 @@
 import requests
+from bs4 import BeautifulSoup
 
 class fakejobscrape:
     def __init__(self):
         self.URL="https://realpython.github.io/fake-jobs/"
         page = requests.get(self.URL)
-        print(page.text)
+        soup = (BeautifulSoup(page.content, "html.parser"))
+        titles = soup.find("h2", class_="title is-5")
+        print(titles.text)
     
 if __name__ == '__main__':
     ob = fakejobscrape()
