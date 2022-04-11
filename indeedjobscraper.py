9eee0be8adaccb45d20b08f6cbaba43e405894cf
Mustafa Siddiqui <mustafa.a.r.siddiqui@outlook.com>
2022-04-10 23:42:19 -0400

    printing company names and job titles

--- indeedjobscraper.py
+++ indeedjobscraper.py
@@ -8,6 +8,11 @@ class jobFinder:
         print('*'*50)
         print(f"{'Welcome To The CLI Job Finder :D':^50}")
         print('~'*50)
+        self.jobs = []
+        self.companies = []
+        self.ratings = []
+        self.locations = []
+        self.descriptions = []
     
     def driver(self):
         self.query = input("What kind of job are you looking for?")
@@ -15,9 +20,21 @@ class jobFinder:
         page = requests.get(f"{self.URL+'q='+self.query+'&l='+self.location}")
         soup = BeautifulSoup(page.content, "html.parser")
         jobs = soup.find_all("h2", class_="jobTitle jobTitle-color-purple")
+        companies = soup.find_all("span", class_="companyName")
+
         for job in jobs:
             temp = job.findChild("span", recursive=False)
+            print(temp)
+            self.jobs.append(temp.text)
             print(temp.text)
+
+        for company in companies:
+            temp = company.findChild("a", recursive=False)
+            try:
+                print(temp.text)
+            except:
+                print(company.text)
+
 
 
 
