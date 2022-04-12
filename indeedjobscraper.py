d027da4a759d9b0508d1848ce02d22df11cb13e7
Mustafa Siddiqui <mustafa.a.r.siddiqui@outlook.com>
2022-04-12 13:53:34 -0400

    Storing job data in an orderly fashion

--- indeedjobscraper.py
+++ indeedjobscraper.py
@@ -3,34 +3,31 @@ from bs4 import BeautifulSoup
 
 class jobFinder:
     def __init__(self):
+        self.start = 0
+        self.jobs = [[],[]]
+
         os.system('cls' if os.name == 'nt' else 'clear')
         self.URL = 'https://ca.indeed.com/jobs?'
         print('*'*50)
         print(f"{'Welcome To The CLI Job Finder :D':^50}")
         print('~'*50)
-        self.jobs = []
-        self.companies = []
-        self.ratings = []
-        self.locations = []
-        self.descriptions = []
-    
+
     def driver(self):
         self.query = input("What kind of job are you looking for?  ->  ")
         self.location = input("What location are you looking to work in? ^-^  ->  ")
         page = requests.get(f"{self.URL+'q='+self.query+'&l='+self.location}")
         soup = BeautifulSoup(page.content, "html.parser")
         jobs = soup.find_all("div", class_="job_seen_beacon")
-
         for job in jobs:
             temp = job.find("h2", recursive=True).find("span", recursive=False)
-            print(temp.text)
+            self.jobs[0].append(temp.text)
             temp = job.find("span", class_="companyName", recursive=True)
             if temp.find("a", recursive=False) == None:
-                print(temp.text)
+                self.jobs[1].append(temp.text)
                 continue
             temp = temp.find("a", recursive=False)
-            print(temp.text)
-        
+            self.jobs[1].append(temp.text)
+        print(self.jobs)
 
 if __name__ == '__main__':
     ob = jobFinder()
